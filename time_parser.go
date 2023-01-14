10c04621ef49696b4ec3892889adcbb88365c725
Robin GPT <robin.chatgpt@gmail.com>
2023-01-15 00:31:10 +0200

    epoch.TimeParse && epoch.TimeParseExt

--- time_parser.go
+++ time_parser.go
@@ -13,32 +13,38 @@ type TimeParser struct {
 
 // Parse attempts to parse the given string using the list of parsers.
 func (tp *TimeParser) Parse(s string, locArg ...*time.Location) (time.Time, error) {
+	t, _, err := tp.ParseExt(s, locArg...)
+	return t, err
+}
+
+// ParseExt attempts to parse the given string using the list of parsers.
+func (tp *TimeParser) ParseExt(s string, locArg ...*time.Location) (time.Time, *ParseDetails, error) {
 	inputs := strings.Split(s, ",")
-	t, err := tp.parseTime(inputs[0], locArg...)
+	t, details, err := tp.parseTime(inputs[0], locArg...)
 	if err != nil {
-		return time.Time{}, fmt.Errorf("failed to parse time: %w", err)
+		return time.Time{}, nil, fmt.Errorf("failed to parse time: %w", err)
 	}
 
 	// if we have just one input, not
 	if len(inputs) == 1 {
-		return t, nil
+		return t, details, nil
 	}
 
 	// comma-separated value are allowed only if interval arithmetics is enabled
 	if !tp.IntervalArithmeticsEnabled {
-		return time.Time{}, fmt.Errorf("unsupported time format")
+		return time.Time{}, nil, fmt.Errorf("unsupported time format")
 	}
 
 	for i := 1; i < len(inputs); i++ {
 		interval, err := ParseInterval(inputs[i])
 		if err != nil {
-			return time.Time{}, fmt.Errorf("failed to parse interval [%s]: %w", inputs[i], err)
+			return time.Time{}, nil, fmt.Errorf("failed to parse interval [%s]: %w", inputs[i], err)
 		}
 
 		t = TimeAddInterval(t, interval)
 	}
 
-	return t, nil
+	return t, details, nil
 }
 
 func NewTimeParser(parsers ...Parser) *TimeParser {
@@ -48,16 +54,16 @@ func NewTimeParser(parsers ...Parser) *TimeParser {
 	return &TimeParser{parsers: parsers}
 }
 
-func (tp *TimeParser) parseTime(s string, locArg ...*time.Location) (time.Time, error) {
+func (tp *TimeParser) parseTime(s string, locArg ...*time.Location) (time.Time, *ParseDetails, error) {
 	for _, parser := range tp.parsers {
 		if !parser.Match(s) {
 			continue
 		}
-		t, _, err := parser.Parse(s, locArg...)
+		t, details, err := parser.Parse(s, locArg...)
 		if err != nil {
-			return time.Time{}, fmt.Errorf("failed to parse time: %w", err)
+			return time.Time{}, nil, fmt.Errorf("failed to parse time: %w", err)
 		}
-		return t, nil
+		return t, details, nil
 	}
-	return time.Time{}, fmt.Errorf("unsupported time format")
+	return time.Time{}, nil, fmt.Errorf("unsupported time format")
 }
